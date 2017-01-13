2c9d13813e6ba185abe434d661a7f84487c48a00
Chris Riley <chris.riley@digital.justice.gov.uk>
2017-01-13 15:00:34 +0000

    Release prep

---
+++ README.md
@@ -0,0 +1,12 @@
+# Zend Auth JWT module
+
+This module provides authentication storage via JWT 
+
+## Installation
+
+Composer require carnage/jwt-zend-auth
+
+Copy jwtzendauth.global.php to your autoload config directory and 
+change the settings as appropriate.
+
+Enable the module Carnage\JwtZendAuth in your application config
---
+++ jwtzendauth.global.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'jwt_zend_auth' => [
+        // Choose signing method for the tokens
+        'signer' => \Lcobucci\JWT\Signer\Hmac\Sha256::class,
+        /*
+            You need to specify either a signing key or set read only to true.
+            If tokens are read only, the implementation will not automatically
+            refresh tokens which are close to expiry so you will need to handle
+            this yourself.
+        */
+        'readOnly' => false,
+        // Set the key to sign the token with, value is dependent on signer set.
+        'signKey' => '',
+        // Set the key to verify the token with, value is dependent on signer set.
+        'verifyKey' => '',
+        /*
+            Default expiry for tokens. A token will expire after not being used
+            for this number of seconds. A token which is used will automatically
+            be extended provided a sign key is provided.
+        */
+        'expiry' => 600
+    ]
+];
